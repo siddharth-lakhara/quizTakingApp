bb39aea362c2c80b3d92b438884b7ec0bd9ba21f
lakhara <siddharth.lakhara@gmail.com>
2018-03-17 11:24:46 +0530

    combined responses from both url

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const ping = require('./ping');
 const devQuestions = require('./dev-questions');
 const devAnswers = require('./dev-answers');
+const devCombine = require('./dev-combine');
 
-module.exports = [].concat(ping, devQuestions, devAnswers);
+module.exports = [].concat(ping, devQuestions, devAnswers, devCombine);
