28f97495213e16fcf14b92684c0d7c5832a600e4
lakhara <siddharth.lakhara@gmail.com>
2018-03-18 15:46:14 +0530

    added joi validation

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,23 @@
 const Models = require('../models');
+const Joi = require('joi');
+// const LoginSwagger = require('../swaggerSpecs/login');
 
 module.exports = [{
   method: 'POST',
   path: '/login',
+  config: {
+    description: 'User Login',
+    notes: 'Created new user automatically',
+    tags: ['api'],
+    // plugins: {
+    //   'hapi-swagger': LoginSwagger,
+    // },
+    validate: {
+      payload: {
+        userName: Joi.string().min(1).max(10).error(new Error('UserName length must be between 0 and 10 characters')),
+      },
+    },
+  },
   handler: (req, reply) => {
     const { userName } = req.payload;
     Models.users.findOrCreate({ where: { username: userName } })
@@ -14,7 +29,7 @@ module.exports = [{
           ).then(() => {
           });
         }
-        reply();
+        reply({ message: 'Done' });
       });
   },
 }];
